efdee5d347c3343bad6775a46a23dcbbad7fc13d
tianchoy <tianchoy@live.com>
2026-05-23 19:42:44 +0800

    fix:test component

--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -3,14 +3,14 @@ const common_vendor = require("../../common/vendor.js");
 if (!Array) {
   const _easycom_m_icon_1 = common_vendor.resolveComponent("m-icon");
   const _easycom_m_button_1 = common_vendor.resolveComponent("m-button");
-  const _easycom_m_popup_1 = common_vendor.resolveComponent("m-popup");
-  (_easycom_m_icon_1 + _easycom_m_button_1 + _easycom_m_popup_1)();
+  const _easycom_m_bottom_popup_1 = common_vendor.resolveComponent("m-bottom-popup");
+  (_easycom_m_icon_1 + _easycom_m_button_1 + _easycom_m_bottom_popup_1)();
 }
 const _easycom_m_icon = () => "../../uni_modules/m-unix/components/m-icon/m-icon.js";
 const _easycom_m_button = () => "../../uni_modules/m-unix/components/m-button/m-button.js";
-const _easycom_m_popup = () => "../../uni_modules/m-unix/components/m-popup/m-popup.js";
+const _easycom_m_bottom_popup = () => "../../uni_modules/m-unix/components/m-bottom-popup/m-bottom-popup.js";
 if (!Math) {
-  (_easycom_m_icon + _easycom_m_button + _easycom_m_popup)();
+  (_easycom_m_icon + _easycom_m_button + _easycom_m_bottom_popup)();
 }
 const _sfc_main = /* @__PURE__ */ common_vendor.defineComponent({
   __name: "index",
@@ -22,23 +22,22 @@ const _sfc_main = /* @__PURE__ */ common_vendor.defineComponent({
       const __returned__ = {
         a: common_vendor.t(common_vendor.unref(title)),
         b: common_vendor.p({
-          name: "print",
+          name: "download",
           size: "40rpx"
         }),
         c: common_vendor.o(($event) => {
           return show.value = true;
-        }, "3a"),
+        }, "16"),
         d: common_vendor.p({
           type: "primary"
         }),
         e: common_vendor.o(($event) => {
           return show.value = false;
-        }, "d2"),
+        }, "21"),
         f: common_vendor.p({
           show: common_vendor.unref(show),
-          position: "bottom",
-          round: true,
-          closeable: true
+          height: 600,
+          radius: true
         }),
         g: common_vendor.sei(common_vendor.gei(_ctx, ""), "view"),
         h: `${_ctx.u_s_b_h}px`,
